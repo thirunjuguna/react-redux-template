8255ce8773d535cc00aa1095342f3d2ad456ea8d
Thiru Njuguna <thirunjuguna@outlook.com>
2019-05-13 11:43:58 +0300

    Adds eslint (#26)

--- src/components/Example/Example.js
+++ src/components/Example/Example.js
@@ -1,60 +1,59 @@
-import React, { Component,PropTypes } from 'react'
-import {connect } from 'react-redux';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import * as exampleActions from '../../actions/exampleActions';
 
+function exampleRow(example, index) {
+  return <div key={index}>{example.name}</div>;
+}
 class Example extends Component {
-
   constructor(props, context) {
     super(props, context);
     this.state = {
       example: {
-        name: ''
-      }
+        name: '',
+      },
     };
     this.onClickSave = this.onClickSave.bind(this);
 
     this.onNameChange = this.onNameChange.bind(this);
-
   }
 
   onNameChange(e) {
-    const example = this.state.example;
+    const { example } = this.state;
     example.name = e.target.value;
-    this.setState({example: example});
+    this.setState({ example });
   }
 
   onClickSave() {
-    this.props.dispatch(exampleActions.createExample(this.state.example));
-  
-  }
-
-  exampleRow(example,index){
-return <div key={index}>{example.name}</div>;
+    const { dispatch } = this.props;
+    const { example } = this.state;
+    dispatch(exampleActions.createExample(example));
   }
 
   render() {
+    const { example } = this.state;
+    const { examples } = this.props;
     return (
       <div>
         <h1>Examples</h1>
-        {this.props.examples.map(this.exampleRow)}
+        {examples.map(exampleRow)}
         <h2>Add New Example</h2>
-        <input
-          type="text"
-          onChange={this.onNameChange}
-          value={this.state.example.name}/>
-        <input
-          type="submit"
-         onClick={this.onClickSave}
-          value="Save"/>
+        <input type="text" onChange={this.onNameChange} value={example.name} />
+        <input type="submit" onClick={this.onClickSave} value="Save" />
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps(state,ownProps){
-  return{
-    examples:state.examples
+function mapStateToProps(state) {
+  return {
+    examples: state.examples,
   };
 }
+Example.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  examples: PropTypes.array.isRequired,
+};
 
 export default connect(mapStateToProps)(Example);
